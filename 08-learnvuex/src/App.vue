dc2f3ca683c2b458434ad4a32f5f0e033d927796
Pzyo <925975498@qq.com>
2021-12-08 16:24:10 +0800

    vuex

--- 08-learnvuex/src/App.vue
+++ 08-learnvuex/src/App.vue
@@ -4,17 +4,32 @@
 		<h2>{{$store.state.counter}}</h2>
 		<button type="button" @click="addition">+</button>
 		<button type="button" @click="subtraction">-</button>
+		<button type="button" @click="addCounter(5)">+5</button>
+		<button type="button" @click="addCounter(10)">+10</button>
 		<h2>---HelloVuex内容---</h2>
 		<hello-vuex/>
 		
 		<h2>---App内容: Getters相关信息---</h2>
 		<h2>{{$store.getters.powerCounter}}</h2>
 		<h2>{{$store.getters.isBig(80)}}</h2>
+		
+		<h2>---App内容: state中info信息的内容是否响应式---</h2>
+		<h2>{{$store.state.info}}</h2>
+		<button type="button" @click="updateInfo">更新信息</button>
+		
+		<h2>---App内容: modules中的内容---</h2>
+		<h2>{{$store.state.moduleA.name}}</h2>
+		<button type="button" @click="$store.commit('updateName', 'zhangsan')">修改名字</button>
+		<h2>{{$store.getters.fullName}}</h2>
+		<h2>{{$store.getters.fullName2}}</h2>
+		<h2>{{$store.getters.fullName3}}</h2>
+		<button type="button" @click="$store.dispatch('aUpdateName')">异步修改名字</button>
   </div>
 </template>
 
 <script>
 	import HelloVuex from './components/HelloVuex.vue'
+	import {INCREMENT} from './store/mutation-types.js'
 	
 	export default {
 		name: 'App',
@@ -23,10 +38,24 @@
 		},
 		methods: {
 			addition() {
-				this.$store.commit('increment')
+				this.$store.commit(INCREMENT)
 			},
 			subtraction() {
 				this.$store.commit('decrement')
+			},
+			addCounter(num) {
+				this.$store.commit({
+					type: 'incrementNum',
+					num
+				})
+			},
+			updateInfo() {
+				// this.$store.commit('updateInfo')
+				this.$store
+					.dispatch('aupdateInfo', '我是playload')
+					.then(res => {
+						console.log(res)
+					})
 			}
 		}
 	}
--- 08-learnvuex/src/store/index.js
+++ 08-learnvuex/src/store/index.js
@@ -1,25 +1,97 @@
 import Vue from "vue"
 import Vuex from "vuex"
 
+import {INCREMENT} from './mutation-types.js'
+
 // 1. 安装插件
 Vue.use(Vuex)
 
 // 2. 创建对象
+const moduleA = {
+	state: {
+		name: 'moduleAsss'
+	},
+	mutations: {
+		updateName(state, playload) {
+			state.name = playload
+		}
+	},
+	getters: {
+		fullName(state) {
+			return state.name + '111'
+		},
+		fullName2(state, getters) {
+			return getters.fullName + '222'
+		},
+		// 模块的getters支持传入三个参数
+		// state：该模块的state
+		// getters：该模块的getters
+		// rootState：根模块的state
+		fullName3(state, getters, rootState) {
+			return getters.fullName2 + rootState.counter
+		}
+	},
+	actions: {
+		// context是一个对象，里面不仅包含本模块的state等信息，还包含rootState等信息
+		// aUpdateName(context) {
+		// 	console.log(context)
+		// 	setTimeout(() => context.commit('updateName', 'wangwu'), 1000)
+		// }
+		aUpdateName({commit}) {
+			// console.log(context)
+			setTimeout(() => commit('updateName', 'wangwu'), 1000)
+		}
+	}
+}
+
 const store = new Vuex.Store({
 	state: {
-		counter: 100
+		counter: 100,
+		info: {
+			name: 'pzyo',
+			age: 24,
+			height: 170
+		}
 	},
 	mutations: {
 		// 方法
 		// state会自动传入
-		increment(state) {
+		[INCREMENT](state) {
 			state.counter++
 		},
 		decrement(state) {
 			state.counter--
+		},
+		incrementNum(state, playload) {
+			state.counter += playload.num
+		},
+		updateInfo(state) {
+			// 非响应式
+			// state.info['addr'] = 'China'
+			// delete state.info.age
+			
+			// 响应式
+			// Vue.set(state.info, 'addr', 'China')  // 新增
+			// Vue.delete(state.info, 'age')  // 删除
+			
+			// 错误：不能使用异步操作
+			// setTimeout(() => {
+			// 	state.info.name = 'tomas'
+			// }, 1000)
+			state.info.name = 'tomas'
 		}
 	},
-	actions: {},
+	actions: {
+		// context：上下文
+		aupdateInfo(context, playload) {
+			return new Promise((resolve, reject) => {
+				setTimeout(() => {
+					context.commit('updateInfo')
+					resolve(playload)
+				}, 1000)
+			})
+		}
+	},
 	getters: {
 		powerCounter(state) {
 			return state.counter * state.counter
@@ -28,7 +100,9 @@ const store = new Vuex.Store({
 			return num => state.counter > num
 		}
 	},
-	modules: {}
+	modules: {
+		moduleA
+	}
 })
 
 // 3. 导出
